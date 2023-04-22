60eadc8654f3a92941eab9940bcdd6c6a400509c
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2023-04-22 20:28:15 +0100

    commit

--- src/Frontend/Pages/BookRoom.jsx
+++ src/Frontend/Pages/BookRoom.jsx
@@ -5,7 +5,63 @@ import Header from "../../Components/Header";
 import FAQMenu from "../Components/FAQMenu";
 import { useGlobalContext } from "../../Function/Context";
 
+const initialState = {
+  username: "",
+  email: "",
+  phone: "",
+  subject: "",
+  description: "",
+};
+
 const BookRoom = () => {
+  const {
+    setloader,
+
+    showNewsletter,
+    notification,
+    notificationF,
+  } = useGlobalContext();
+
+  const [form, setform] = useState(initialState);
+
+  const [dateId, setdateId] = useState("");
+
+  const { username, email, phone, subject, description } = form;
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    const dateId = new Date().getTime();
+    setdateId(dateId);
+  }, []);
+
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (username && email && phone && description) {
+      setloader(true);
+      try {
+        await addDoc(collection(db, "Messages"), {
+          ...form,
+
+          timestamp: serverTimestamp(),
+        });
+        await addDoc(collection(db, "Newsletter"), {
+          email: email,
+        });
+        setloader(false);
+        toast.success("Message Sent");
+      } catch (error) {
+        console.log(error);
+        notificationF(error);
+      }
+    } else {
+      return toast.error("All fields must be filled");
+    }
+  };
+
   const { loader, Rooms } = useGlobalContext();
 
   const { id } = useParams();
@@ -42,40 +98,91 @@ const BookRoom = () => {
 
             <section className="my-20 flex flex-col mx-56">
               <>
-                <h1 className="text-6xl ]  pb-2 ">Room Details</h1>
+                <h1 className="text-6xl ]  pb-2 ">Book Room</h1>
 
                 <div className="h-[2px] w-48 mb-1 bg-black"></div>
                 <div className="h-[2px] w-20 bg-black"></div>
 
-                <p className="mt-12">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Debitis eveniet sed magnam consectetur animi qui hic delectus
-                  facilis natus impedit velit voluptatibus officiis, voluptate
-                  quaerat placeat est asperiores libero vitae eos, harum quia.
-                  Officiis nemo earum, quaerat eaque ducimus facere, repellat,
-                  expedita dolorum ea excepturi voluptatibus odio consequuntur
-                  suscipit. Quae ut laudantium repudiandae magnam? Beatae
-                  molestias nemo aperiam laudantium a omnis eaque iusto
-                  laboriosam error assumenda! Assumenda accusamus maiores
-                  nostrum cupiditate natus quis odit alias quae aspernatur
-                  totam, est quia in molestiae blanditiis consectetur. Aperiam
-                  voluptates ab veniam sunt vel! Corrupti provident quia est
-                  minus eaque quidem dolorem nobis facere. <br />
-                  <br /> Lorem ipsum dolor, sit amet consectetur adipisicing
-                  elit. Nobis non a, facere temporibus repudiandae ut officia
-                  totam optio eius ipsum deleniti ratione quaerat qui culpa
-                  laboriosam cupiditate doloribus impedit excepturi dolorem
-                  dicta distinctio molestias repellendus. Minus labore quod
-                  laborum aspernatur cupiditate. Odit expedita ipsa corrupti
-                  reiciendis ducimus necessitatibus cum voluptates ea deleniti
-                  natus alias quas rem, amet neque officiis ab incidunt ex
-                  voluptatum veritatis eum! Eligendi consectetur blanditiis
-                  placeat itaque ea fugiat quod? Debitis rerum, provident
-                  facilis dolor deserunt, molestias eveniet quia perspiciatis
-                  nostrum corrupti obcaecati, dolores eaque rem quam ex animi
-                  maiores repellat magnam! Voluptates ad perspiciatis vel
-                  aliquam.
-                </p>
+                <div className="flex  flex-col  my-[30px] md:px-[200px] px-[20px] text-[14px]">
+                  <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 my-[10px]">
+                    <input
+                      type="text"
+                      name="username"
+                      value={username}
+                      onChange={handleChange}
+                      placeholder="Name"
+                      required
+                      className="border py-[18px] border-black px-[25px] text-[14px] "
+                    />
+                    <input
+                      type="email"
+                      name="email"
+                      value={email}
+                      onChange={handleChange}
+                      placeholder="Email"
+                      required
+                      className="border py-[18px] border-black px-[25px] text-[14px] "
+                    />
+                  </div>
+                  <div className="grid grid-cols-1 sm:grid-cols-2  gap-3 my-[10px]">
+                    <input
+                      type="number"
+                      name="phone"
+                      value={phone}
+                      onChange={handleChange}
+                      placeholder="Phone"
+                      required
+                      className="border py-[18px] border-black px-[25px] text-[14px] "
+                    />
+                    <input
+                      type="text"
+                      name="Room Type"
+                      value={room.RoomName}
+                      placeholder="Room Type"
+                      className="border py-[18px] border-black px-[25px] text-[14px] "
+                    />
+                  </div>
+                  <div className="grid grid-cols-1 sm:grid-cols-2  gap-3 my-[10px]">
+                    <input
+                      type="number"
+                      name="numberofGuest"
+                      //   value={numberofGuest}
+                      onChange={handleChange}
+                      placeholder="Number of Guest"
+                      required
+                      className="border py-[18px] border-black px-[25px] text-[14px] "
+                    />
+                    <input
+                      type="text"
+                      name="Room Type"
+                      value={room.RoomName}
+                      placeholder="Room Type"
+                      className="border py-[18px] border-black px-[25px] text-[14px] "
+                    />
+                  </div>
+                  <div className="grid grid-cols-1 gap-3 my-[10px]">
+                    <textarea
+                      name="description"
+                      value={description}
+                      onChange={handleChange}
+                      placeholder="Message"
+                      required
+                      className="border py-[18px] border-black px-[25px] text-[14px]  
+                    "
+                      rows="10"
+                    />
+                  </div>
+                  <p className="text-center text-[14px] text-red-600">
+                    {notification}
+                  </p>
+
+                  <button
+                    onClick={handleSubmit}
+                    className="text-[13px] bg-transparent m-auto font-semibold my-5  flex justify-center items-center border-[2px] border-black px-[34px] py-[9px] text-black  w-[200px] hover:bg-black hover:text-white"
+                  >
+                    SEND MESSAGE
+                  </button>
+                </div>
 
                 <div className="my-5 space-x-4 mb-14">
                   <Link to="/bookroom">
@@ -88,24 +195,6 @@ const BookRoom = () => {
                       see other rooms
                     </button>
                   </Link>
-                </div>
-
-                <h1 className="text-6xl ]  pb-2 ">Room Images</h1>
-
-                <div className="h-[2px] w-48 mb-1 bg-black"></div>
-                <div className="h-[2px] w-20 bg-black"></div>
-
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mt-11">
-                  <img
-                    src="/slider_1.jpg"
-                    alt="room"
-                    className="w-full h-full"
-                  />
-                  <img
-                    src="/slider_2.jpg"
-                    alt="room"
-                    className="w-full h-full"
-                  />
                 </div>
               </>
             </section>
