3b3726434dedfe7c7cf56b6fd2d428e1ce32ef68
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2023-05-05 08:25:50 +0100

    Update FAQMenu.jsx

--- src/Frontend/Components/FAQMenu.jsx
+++ src/Frontend/Components/FAQMenu.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const FAQMenu = () => {
   return (
-    <div className="md:py-36 py-24  md:px-5 flex items-center justify-center bg-[#e6e6e6]">
+    <div className="md:py-36 py-24  px-5 flex items-center justify-center bg-[#e6e6e6]">
       <h1 className="md:text-3xl text-2xl text-center text-black">
         If you have any special requests, please feel free to{" "}
         <Link className="underline border-b" to="/contact">
           Contact Us
         </Link>{" "}
-        or call us. +234 810 395 1449
+        or call us. +2348103951449
       </h1>
     </div>
   );
